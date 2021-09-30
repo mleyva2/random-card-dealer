aae1578a90ed478a8bcac2db8e983fa1b9c50486
mleyva2 <mleyva@rccl.com>
2021-09-30 00:37:49 +0000

    final commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["♦", "♥", "♠", "♣"];
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let top = document.querySelector(".top-suit");
+  let center = document.querySelector(".number");
+  let bottom = document.querySelector(".bottom-suit");
+
+  let randsuit = Math.floor(Math.random() * 4);
+  const element = suits[randsuit];
+  if (suits[randsuit] === "♥" || suits[randsuit] === "♦") {
+    top.style.color = "red";
+    bottom.style.color = "red";
+  } else {
+    top.style.color = "black";
+    bottom.style.color = "black";
+  }
+
+  top.innerHTML = element;
+  bottom.innerHTML = element;
+
+  let randnumber = Math.floor(Math.random() * 14);
+  const element1 = numbers[randnumber];
+  center.innerHTML = element1;
 };
